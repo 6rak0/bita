ce4e342a9764a426c4100fb7416f301e86176944
6rak0 <ego.6rak0@gmail.com>
2020-12-27 16:46:02 -0600

    commit inicial

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import drive from "drive-db";
+	import { onMount } from "svelte";
+	import Item from "./Item.svelte";
+
+	let db = [];
+
+	onMount(async () => {
+		db = await drive("1w7DdwTiFllZ4Mg4nVWNFF6g2TKnGjsICo9ioFJwcXDw");
+		db.reverse();
+	});
+</script>
+
+<div class="m-3">
+	<h1 class="text-center">
+		<i class="fas fa-virus text-green-700" />
+		evolución
+		<i class="fas fa-virus text-green-700" />
+	</h1>
+</div>
+
+{#each db as item}
+	<Item {item} />
+{/each}
---
+++ src/Item.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let item;
+</script>
+
+<div class="m-3 p-3 shadow rounded">
+  <h3 class="text-center">{item.timestamp}</h3>
+  <div class="py-6 flex justify-around">
+    <div class="p-2">
+      <i class="fas fa-thermometer-half text-blue-400" />
+      {item.temperatura}
+    </div>
+    <div class="p-2">spO² - {item.oxigenación}%</div>
+    <div class="p-2">
+      <i class="fas fa-heartbeat text-red-500" />
+      -
+      {item.sistólica}/{item.diastólica}
+    </div>
+  </div>
+</div>
